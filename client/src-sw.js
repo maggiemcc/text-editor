e965e56425d3c6defa991c7512da1c0203c8f041
Maggie McCausland <maggiemccausland@hotmail.com>
2024-01-22 16:29:42 -0700

    reverted back to previous code to fix getDb issues

--- client/src-sw.js
+++ client/src-sw.js
@@ -36,9 +36,6 @@ registerRoute(
         new CacheableResponsePlugin({
           statuses: [0, 200],
         }),
-        // new ExpirationPlugin({
-        //   maxAgeSeconds: 30 * 24 * 60 * 60,
-        // }),
       ],
     })
 );
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,21 +20,19 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({id: 1, value: content});
   const result = await request;
-  console.log(result);
-
-  return result;
+  return result.value;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('GET from the database::', result);
-
-  return result;
+  return result.value;
 };
+
 
 initdb();
