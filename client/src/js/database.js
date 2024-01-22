4c0dacaeb9194c6edeb3ff793d2d45b1c145188b
Maggie McCausland <maggiemccausland@hotmail.com>
2024-01-22 16:20:29 -0700

    minor changes to package.json and database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,20 +20,21 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({id: 1, value: content});
   const result = await request;
-  console.log('result.value', result.value);
-  return result.value;
+  console.log(result);
+
+  return result;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result.value);
-  return result.value;
+  console.log('GET from the database::', result);
+
+  return result;
 };
 
 initdb();
