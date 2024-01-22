bc4e57e394f7d5abaa98b17dd5aa6c934dcf7722
Maggie McCausland <maggiemccausland@hotmail.com>
2024-01-22 15:23:13 -0700

    minor trouble shooting in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,10 +18,10 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: 1, jate: content});
+  const request = store.put({id: 1, value: content});
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result.value;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -32,8 +32,8 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result.value;
 };
 
 initdb();
